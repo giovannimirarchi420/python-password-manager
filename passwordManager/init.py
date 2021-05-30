12e45d7b38f0bfc87a7d280f61b2011d36ca59df
giovannimirarchi420 <giovanni.mirarchi@hotmail.com>
2021-05-30 16:52:35 +0200

    Added init script to setup the environment before first launch of pswManager

---
+++ passwordManager/init.py
@@ -0,0 +1,7 @@
+import os
+
+print('Installing all dependencies...')
+print('pyAesCrypt it is installing ')
+os.system('pip install pyAesCrypt')
+print('Setup finished, now you can use pswManager tool!')
+
