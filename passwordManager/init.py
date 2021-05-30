3e3b8c8d8eec397aed20f7342c586b600f442659
giovannimirarchi420 <giovanni.mirarchi@hotmail.com>
2021-05-30 16:57:19 +0200

    init.py edited, now it will be removed after init setup

--- passwordManager/init.py
+++ passwordManager/init.py
@@ -4,4 +4,4 @@ print('Installing all dependencies...')
 print('pyAesCrypt it is installing ')
 os.system('pip install pyAesCrypt')
 print('Setup finished, now you can use pswManager tool!')
-
+os.remove('init.py')
